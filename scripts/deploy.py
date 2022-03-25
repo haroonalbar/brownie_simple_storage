995681ab85512db8cb6cd3fc308f61e4a29f7d88
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-03-25 21:58:51 +0530

    go back to the local acc that brownie creates for us and deploy the contract directly by importing it

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,16 +1,19 @@
 # import accounts to work with address and privatekey
-from brownie import accounts, config
+# import SimpleStorage contract
+from brownie import accounts, config, SimpleStorage
 
 
 def deploy_simple_storage():
-    # account = accounts[0]
+    account = accounts[0]
+    simple_storage = SimpleStorage.deploy({"from": account})
+    print(simple_storage)
     # print(account)
 
     # to work with the the account you created
     # account = accounts.load("dog")
     # print(account)
-    account = accounts.add(config["wallet"]["from_key"])
-    print(account)
+    # account = accounts.add(config["wallet"]["from_key"])
+    # print(account)
 
 
 def main():
