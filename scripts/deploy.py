7cc42fa02d52fafc3c3c5c13bc9e3ed696f1e6d8
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-03-25 22:14:34 +0530

    update store from brownie

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -5,9 +5,14 @@ from brownie import accounts, config, SimpleStorage
 
 def deploy_simple_storage():
     account = accounts[0]
+    # in brownie in transcation we have to specify the account we are transactining from
     simple_storage = SimpleStorage.deploy({"from": account})
     stored_value = simple_storage.retrieve()
     print(stored_value)
+    transcation = simple_storage.store(15, {"from": account})
+    transcation.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
     # print(simple_storage)
     # print(account)
 
